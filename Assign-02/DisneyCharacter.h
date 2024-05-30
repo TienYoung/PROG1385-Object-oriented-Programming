c81898da80319c0f697cfc85242229a422789f13
Tien Young <tyang2896@conestogac.on.ca>
2024-05-29 23:33:18 -0400

    Updated DisneyCharacter.
    1. Declare public functions.
    2. Implement mutators.

--- Assign-02/DisneyCharacter.h
+++ Assign-02/DisneyCharacter.h
@@ -1,22 +1,72 @@
 #pragma once
 
 #include <cstring>
+#include <cstdio>
 
 #define MAX_CHARS 50
 
 class DisneyCharacter
 {
 public:
-	DisneyCharacter(const char* name, const char* createDate, int numMovies, char whichPark);
+	DisneyCharacter(const char* name, const char* createDate, int numMovies = 0, char whichPark = 'N');
 	~DisneyCharacter();
 
-	void SetName(const char* name)
+	void ShowInfo(void);
+	bool PlaceCharacter(char whichPark);
+	void SameMovies(DisneyCharacter& anotherCharacter);
+
+
+	void SetName(const char* name) { strcpy_s(this->name, MAX_CHARS, name); }
+
+	bool SetCreationDate(const char* date) 
 	{
-		strcpy_s(this->name, MAX_CHARS, name);
+		char* yyyy = nullptr;
+		char* mm = nullptr;
+		char* dd = nullptr;
+		if (sscanf_s(date, "%s-%s-%s", yyyy, 4, mm, 2, dd, 2) != 3)
+		{
+			return false;
+		}
+		else
+		{
+			// TODO: validate the numbers.
+		}
 	}
-	void SetCreationDate(const char* date);
-	void SetNumMovies(int num);
-	void SetWhichPark(char park);
+	
+	bool SetNumMovies(int num) 
+	{ 
+		bool valid = num >= 0;
+		numMovies = valid ? num : 0;
+		return valid;
+	}
+	
+	bool SetWhichPark(char park)
+	{
+		switch (park)
+		{
+		case 'M':
+			park = 'M';
+			return true;
+        case 'S':
+            park = 'S';
+			return true;
+        case 'A':
+            park = 'A';
+			return true;
+        case 'E':
+            park = 'E';
+			return true;
+        case 'C':
+            park = 'C';
+			return true;
+        case 'N':
+            park = 'N';
+			return true;
+		default:
+			park = 'N';
+			return false;
+		}
+	}
 
 private:
 	char name[50];
