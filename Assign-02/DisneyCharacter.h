cf63c10922b34f5bba8e7dccc9c6c8d6be3b84ec
Tien Young <tyang2896@conestogac.on.ca>
2024-05-31 11:45:51 -0400

    Updated DisneyCharacter header file.
    1. Add a constructor with two parameters. And remove the default parameters of the first constructor.
    2. Add four accessors.

--- Assign-02/DisneyCharacter.h
+++ Assign-02/DisneyCharacter.h
@@ -1,5 +1,3 @@
-#pragma once
-
 #include <cstring>
 #include <cstdio>
 
@@ -8,70 +6,76 @@
 class DisneyCharacter
 {
 public:
-	DisneyCharacter(const char* name, const char* createDate, int numMovies = 0, char whichPark = 'N');
-	~DisneyCharacter();
+    DisneyCharacter(const char* name, const char* createDate, int numMovies, char whichPark);
+    DisneyCharacter(const char* name, const char* createDate);
+    ~DisneyCharacter();
 
-	void ShowInfo(void);
-	bool PlaceCharacter(char whichPark);
-	void SameMovies(DisneyCharacter& anotherCharacter);
+    void ShowInfo(void);
+    bool PlaceCharacter(char whichPark);
+    void SameMovies(DisneyCharacter& anotherCharacter);
 
 
-	void SetName(const char* name) { strcpy_s(this->name, MAX_CHARS, name); }
+    inline const char* GetName() const { return name; };
+    inline const char* GetCreationDate() const{ return creationDate; };
+    inline int GetNumMovies() const { return numMovies; };
+    inline char GetWhichPark() const { return whichPark; };
 
-	bool SetCreationDate(const char* date) 
-	{
-		char* yyyy = nullptr;
-		char* mm = nullptr;
-		char* dd = nullptr;
-		if (sscanf_s(date, "%s-%s-%s", yyyy, 4, mm, 2, dd, 2) != 3)
-		{
-			return false;
-		}
-		else
-		{
-			// TODO: validate the numbers.
-		}
-	}
-	
-	bool SetNumMovies(int num) 
-	{ 
-		bool valid = num >= 0;
-		numMovies = valid ? num : 0;
-		return valid;
-	}
-	
-	bool SetWhichPark(char park)
-	{
-		switch (park)
-		{
-		case 'M':
-			park = 'M';
-			return true;
+    void SetName(const char* name) { strcpy_s(this->name, MAX_CHARS, name); }
+
+    bool SetCreationDate(const char* date) 
+    {
+        char* yyyy = nullptr;
+        char* mm = nullptr;
+        char* dd = nullptr;
+        if (sscanf_s(date, "%s-%s-%s", yyyy, 4, mm, 2, dd, 2) != 3)
+        {
+            return false;
+        }
+        else
+        {
+            // TODO: validate the numbers.
+        }
+    }
+    
+    bool SetNumMovies(int num) 
+    { 
+        bool valid = num >= 0;
+        numMovies = valid ? num : 0;
+        return valid;
+    }
+    
+    bool SetWhichPark(char park)
+    {
+        switch (park)
+        {
+        case 'M':
+            park = 'M';
+            return true;
         case 'S':
             park = 'S';
-			return true;
+            return true;
         case 'A':
             park = 'A';
-			return true;
+            return true;
         case 'E':
             park = 'E';
-			return true;
+            return true;
         case 'C':
             park = 'C';
-			return true;
+            return true;
         case 'N':
             park = 'N';
-			return true;
-		default:
-			park = 'N';
-			return false;
-		}
-	}
+            return true;
+        default:
+            park = 'N';
+            return false;
+        }
+    }
 
 private:
-	char name[50];
-	char creationDate[50];
-	int numMovies;
-	char whichPark;
+    char name[50];
+    char creationDate[50];
+    int numMovies;
+    char whichPark;
 };
 
